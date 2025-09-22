9fbee966e97a3ad64331ced3f7cf7a0f4a7d3e15
Kiproy04 <delleroy04@gmail.com>
2025-09-22 15:20:06 +0300

    Updated views implementation and url configurations

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated 
 from .models import Message, Conversation
@@ -32,6 +32,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
 
     def get_queryset(self):
         conversation_id = self.request.query_params.get("conversation_id")
