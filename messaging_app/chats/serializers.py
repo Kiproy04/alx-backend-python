c11cc2ec2efea9335b079e5533fd2baae00fac22
Kiproy04 <delleroy04@gmail.com>
2025-09-22 14:27:38 +0300

    Updated serializers implementation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
-from . models import User, Message, Conversation
+from .models import User, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source="get_full_name", read_only=True)
+    
     class Meta:
         model = User
         fields = '__all__'
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField() 
+
     class Meta:
         model = Conversation
         fields = '__all__'
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Message
         fields = '__all__'
