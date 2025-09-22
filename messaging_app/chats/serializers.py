706ab08cd359a8b2c34ee65d75ece24c32d9c570
Kiproy04 <delleroy04@gmail.com>
2025-09-22 14:29:08 +0300

    Updated serializers implementation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,7 +3,7 @@ from .models import User, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source="get_full_name", read_only=True)
-    
+
     class Meta:
         model = User
         fields = '__all__'
@@ -22,3 +22,9 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+    def validate(self, data):
+        participants = self.initial_data.get("participants", [])
+        if len(participants) < 2:
+            raise serializers.ValidationError("A conversation must have at least 2 participants.")
+        return data
