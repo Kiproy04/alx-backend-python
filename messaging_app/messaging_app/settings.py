4b8d3275ad27d920e99e6a3a58c3634530ab718a
Kiproy04 <delleroy04@gmail.com>
2025-09-24 16:20:01 +0300

    Pagination and Filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
